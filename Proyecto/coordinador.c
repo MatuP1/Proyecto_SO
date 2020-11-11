68b87ec6da55ebe0c99f4e55f4d700907b2a3b2b
Mati <matiaspippig@gmail.com>
2020-11-11 11:56:43 -0300

    Actualizacion de ccordicando

--- Proyecto/coordinador.c
+++ Proyecto/coordinador.c
@@ -8,58 +8,56 @@
 #define TIME_UNIT 1
 
 
-struct info_task {
-int tipe_work;
-char *dato;
+struct task {
+	int type_work;
+	char dato[25];
 };	
-typedef struct info_task tInfo; // estructura de la info necesaria para las tareas
-struct tipe_task {
-	char tipe;
-	tInfo info;
-}; typedef struct tipe_task tTask; // estructura de los mensajes de creador de tareas
+typedef struct task *tTask; // estructura de las tareas
+struct info_task {
+	char type;
+	struct task tarea;
+}; typedef struct info_task *tInfo; // estructura de los mensajes del creador de tareas
 
-#define SIZE_MSG sizeof(tMessage)
+#define SIZE_MSG sizeof(struct info_task)
 #define MIN_TASK 4
 #define MAX_TASK 6
 
-int pipe_t[2]; //pipe para comunicar las tareas
-int pipe_co[2];//pipe para coordinar
+int pipe_a[2];
+int pipe_b[2];
+int pipe_c[2];
+int pipe_tareas[2]; //pipe para comunicar las tareas
+int pipe_coordinador[2];//pipe para coordinar
 
 void creador_tareas(int cant_tareas){
-	close(pipe_t[0]);
-	tTask *tarea_nueva;
+	close(pipe_tareas[0]);
+	tInfo tarea_info;
 	int i = 0;
 	while(i<cant_tareas){
-		tarea_nueva = malloc(sizeof(tTask));
-		char valor_d[25];
-		tarea_nueva->info.dato = valor_d;
+		tarea_info = malloc(sizeof(struct info_task));
 		printf("Ingrese el tipo de tarea: ");
-		scanf("%s",&tarea_nueva->tipe);
-		printf("Tipo de tarea ingresado; %c\n",tarea_nueva->tipe);
+		scanf("%s",&(tarea_info->type));
+		printf("Tipo de tarea ingresado; %c\n",tarea_info->type);
 		printf("Ingrese el tipo de trabajo: ");
-		scanf("%d",&tarea_nueva->info.tipe_work);
-		printf("Tipo de trabajo ingresado: %d\n",tarea_nueva->info.tipe_work);
+		scanf("%d",&(tarea_info->tarea.type_work));
+		printf("Tipo de trabajo ingresado: %d\n",tarea_info->tarea.type_work);
 		printf("Ingrese el Dato para el trabajo: ");
-		scanf("%s",tarea_nueva->info.dato);
-		printf("Dato Ingresado para el trabajo: %s\n",tarea_nueva->info.dato);
-		
+		scanf("%s",tarea_info->tarea.dato);
+		printf("Dato Ingresado para el trabajo: %s\n",tarea_info->tarea.dato);
 		i++;
-		write(pipe_t[1],tarea_nueva,sizeof(tTask));
-		free(tarea_nueva);
+		write(pipe_tareas[1],tarea_info,sizeof(struct info_task));
+		free(tarea_info);
 	}
 }
 
 void tarea_a(){
-	printf("Cierro el pipe en la tarea a\n");
-	close(pipe_co[1]);
-	tInfo *datos = malloc(sizeof(tInfo));
+	tTask tarea_actual = malloc(sizeof(struct task));
 	printf("Reservo memoria en la tarea a\n");
-	read(pipe_co[0],datos,sizeof(tInfo));
+	read(pipe_a[0],tarea_actual,sizeof(struct task));
 	printf("Leo sin errores en la tarea a\n");
-	char* color = datos->dato;
-	printf("El color en la tarea a es :%s\n",datos->dato);
-	int tarea = datos->tipe_work;
-	printf("La tarea solicitada es la numero %d en la tarea a\n", datos->tipe_work);
+	char* color = tarea_actual->dato;
+	printf("El color en la tarea a es :%s\n",tarea_actual->dato);
+	int tarea = tarea_actual->type_work;
+	printf("La tarea solicitada es la numero %d en la tarea a\n", tarea_actual->type_work);
 	if(tarea == 1){ // tarea parcial
 		printf("Duermo una Unidad de Tiempo en A a\n");
 		sleep(1*TIME_UNIT);
@@ -71,54 +69,57 @@ void tarea_a(){
 		printf("Despierto en A a\n");
 	}
 	printf("Auto pintado de color %s\n",color);
-	free(datos);
+	free(tarea_actual);
+	int valor = 1;
+	write(pipe_coordinador[1],&valor,sizeof(int));
 }
 void tarea_b(){
-	close(pipe_co[1]);
-	tInfo *datos = malloc(sizeof(tInfo));
-	int termine=read(pipe_co[0],&datos,sizeof(tInfo));
+	tTask datos = malloc(sizeof(tInfo));
+	int termine=read(pipe_b[0],&datos,sizeof(tInfo));
 	if(termine == 0)
-		printf("se ejecuto tarea A\n");
+		printf("se ejecuto tarea B\n");
 	char* ver_rep = datos->dato;
-	int tarea = datos->tipe_work;
+	int tarea = datos->type_work;
 	if(tarea == 1){ // tarea verificar
 		sleep(1*TIME_UNIT);
 	}
 	else
 		sleep(2*TIME_UNIT);
 	printf("La %s de los frenos del Auto se realizo correctamente\n",ver_rep);
+	int valor = 1;
+	write(pipe_coordinador[1],&valor,sizeof(int));
 }
 void tarea_c(){
-	close(pipe_co[1]);
-	tInfo *datos = malloc(sizeof(tInfo));
-	read(pipe_co[0],&datos,sizeof(tInfo));
+	tTask datos = malloc(sizeof(tInfo));
+	read(pipe_c[0],&datos,sizeof(tInfo));
 	char* rep_bal = datos->dato;
-	int tarea = datos->tipe_work;
+	int tarea = datos->type_work;
 	if(tarea == 1){ // tarea balanceo
 		sleep(3*TIME_UNIT);
 	}
 	else
 		sleep((tarea*TIME_UNIT)-TIME_UNIT); // la tarea son las ruedas a reparar mas uno 
 	printf("La %s de las ruedas del Auto se talizo correctamente\n",rep_bal);
+	int valor = 1;
+	write(pipe_coordinador[1],&valor,sizeof(int));
 }
 void coordinador(int cant_tareas){
-	close(pipe_co[0]);
-	close(pipe_t[1]);
+	close(pipe_tareas[1]);
 	if (cant_tareas >= MIN_TASK && cant_tareas <= MAX_TASK){
 		int i = 0;
-		tTask *tipo_task;
+		tInfo tipo_task;
 		while (i<cant_tareas){
-			read(pipe_t[0],&tipo_task,sizeof(tTask));
-			printf("Leo una tarea de tipo:%c\n",(tipo_task->tipe));
-			if(tipo_task->tipe == 'a'){
-				write(pipe_co[1],&tipo_task->info,sizeof(tInfo));
+			read(pipe_tareas[0],&tipo_task,sizeof(struct info_task));
+			printf("Leo una tarea de tipo:%c\n",(tipo_task->type));
+			if(tipo_task->type == 'a'){
+				write(pipe_a[1],&tipo_task->tarea,sizeof(struct task));
 			}
 			else
-				if(tipo_task->tipe == 'b'){
-					write(pipe_co[1],&tipo_task->info,sizeof(tInfo));
+				if(tipo_task->type == 'b'){
+					write(pipe_b[1],&tipo_task->tarea,sizeof(tInfo));
 				}
 				else{ // la tarea es de tipo c
-					write(pipe_co[1],&tipo_task->info,sizeof(tInfo));
+					write(pipe_c[1],&tipo_task->tarea,sizeof(tInfo));
 				}
 			i++;
 		}
@@ -130,10 +131,17 @@ void coordinador(int cant_tareas){
 
 int main(){
 	printf("Arranca la locomotora del sabor\n");
-	if(pipe(pipe_t) == -1){
+	if(pipe(pipe_tareas) == -1){
 		perror("Fallo al crear el pipe");
 	}
-	if(pipe(pipe_co) == -1){
+	if(pipe(pipe_coordinador) == -1){
+		perror("Fallo al crear el pipe");
+	}
+	if(pipe(pipe_a) == -1){
+		perror("Fallo al crear el pipe");
+	}if(pipe(pipe_b) == -1){
+		perror("Fallo al crear el pipe");
+	}if(pipe(pipe_c) == -1){
 		perror("Fallo al crear el pipe");
 	}
 	pid_t pid_ta = fork();
@@ -169,21 +177,21 @@ int main(){
 							// recibis tareas indefinidamente
 							int tareas = 10;
 							int i = 0;
+							int termine = 0;
 							while (i < tareas){
 								tareas = ((random() % 3) + 4);
 								creador_tareas(tareas);
+								coordinador(tareas);
 								printf("%d\n",tareas);
 								i++;
-								coordinador(tareas);
-								waitpid(pid_ta,NULL,0);
-								waitpid(pid_tb,NULL,0);
-								waitpid(pid_tc,NULL,0);
+								while(termine!= tareas){
+									read(pipe_coordinador[0],NULL,sizeof(int));
+									termine++;
+								}
+								termine = 0;
 							}
 							
 						}
 					}
 			}
 }
-	
-//HAY QUE HACERLO COMO DIJO SANTI, EL TENIA RAZON Y YO ESTABA EQUIVOCADO CHUPA VERGA DE MIERDA, A PESAR DE SU AMOR POR EL PENE MASCULINO TENIA RAZON
-
