46f2d9edbd664d92d7ae8fa54976d0e48b99a9b3
Santiago Rosales Guinzburg <54337526+SaintRoguer@users.noreply.github.com>
2020-11-16 13:15:32 -1000

    Update modPer
    Añadida la verificacion de que el modo introducido es valido.

--- Proyecto/MiniShell.c
+++ Proyecto/MiniShell.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <limits.h>
 
 
 //Funcion que crea un directorio
@@ -68,12 +69,40 @@ void mostrarContenido(char *nombre){
 }
 //Funcion que modifica los permisos de un archivo.
 void modificarPermisos(char *nombre, char *modo){
+	
+	
+	//Reset errno a 0 antes de la llamada
+	errno=0;
+	int largo = strlen(modo);
+	char *endptr = NULL;
 	int i;
-	i= strtol(modo, 0, 8);
-	if(chmod(nombre,i)<0)
-		perror("Error en chmod");
+	i= strtol(modo, &endptr, 8);
+	//Verifico que modo es un numero octal (0+3 cifras)
+	if(modo == endptr || largo != 4)
+		printf("El modo ingresado no corresponde a un numero octal de 4 cifras.\n");
 	else
-		printf("Se cambiaron los permisos del archivo %s",nombre);
+		if( i<0 || i>999)
+			printf("El modo ingresado no corresponde a un numero octal de 4 cifras.\n");
+		else
+			if(errno == ERANGE && i ==LONG_MIN)
+				printf("Ocurrio un underflow.\n");
+			else
+				if(errno == ERANGE && i == LONG_MAX)
+					printf("Ocurrio un overflow.\n");
+				else
+					if(errno == ERANGE)
+						printf( "base contiene valor no soportado.\n");
+					else
+						if(errno != 0 && i == 0)
+							printf("Ocurrio un error no especificado.\n");
+						else
+							if(errno == 0 && modo && *endptr != 0)
+								printf("Contiene un modo pero tambien contiene caracteres.\n");
+							else
+								if(chmod(nombre,i)<0)
+									perror("Error en chmod");
+								else
+									printf("Se cambiaron los permisos del archivo %s.\n",nombre);
 	
 }
 
