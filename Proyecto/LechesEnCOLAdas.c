7bf282c13652865b47881200540da28446d08033
SaintRoguer <anonymousvsworld@gmail.com>
2020-11-18 20:44:50 -1000

    Creo que funciona

--- Proyecto/LechesEnCOLAdas.c
+++ Proyecto/LechesEnCOLAdas.c
@@ -23,7 +23,7 @@
 struct message{
 	
 	long type;
-	char instrLeche[40];
+	
 }; typedef struct message SMS;
 
 void report_and_exit(const char* msg){
@@ -47,48 +47,42 @@ void comprarLeche(int C, int tiempo){
 	sleep(Tespera);
 }
 
-void companiero(int tiempo,int compa,int queue,key_t key,int cantCompanieros){
+void companiero(int tiempo,int compa,int queue,int cantCompanieros){
 		printf("Nacio el compañero : %i\n", compa);
-		errno = 0;
 		SMS messRCV;
 		SMS messSNDT1;
 		SMS messSNDT2;
-		SMS messSNDT3;
 		int tiempoActual;
 		sleep(Tespera);
 		while(1){
-			int recibio = msgrcv(queue,&messRCV,SIZE_MSG,TYPE_2,IPC_NOWAIT);//Hay leche?
-			if (recibio > -1){//Hay leche y me tomo 1.
+			int recibio = msgrcv(queue,&messRCV,SIZE_MSG,TYPE_2,IPC_NOWAIT);//NO Hay leche?
+			if (recibio > -1){//Hay leche y la toma.
 				tiempoActual= time(NULL) -tiempo; 
-				printf("%i : %i : Hay leche, y toma 1.\n",tiempoActual, compa);
-				sleep(Tespera);
+				printf("%i : %i : Hay leche, y la toma.\n",tiempoActual, compa);
+				sleep(2*Tespera);//Toma la leche
 				messSNDT1.type = TYPE_1;
 				msgsnd(queue, &messSNDT1, SIZE_MSG,0);
-				sleep(Tespera);
+				
 			}
 			else{//No hay leche
-				sleep(Tespera);
-				sleep(Tespera);
+				sleep(Tespera);//Revisa la heladera.
+				tiempoActual= time(NULL) -tiempo;	
+				printf("%i : %i : Mira el refrigerador; no hay leche.\n",tiempoActual,compa);
 				int comprar = msgrcv(queue,&messRCV,SIZE_MSG,TYPE_1,IPC_NOWAIT);
 				if(comprar > -1){//Alguien fue a comprar?
 					//Va a comprar
-					sleep(Tespera);
-					tiempoActual= time(NULL) -tiempo;
-					printf("%i : %i : Mira el refrigerador; no hay leche.\n",tiempoActual,compa);
+					sleep(Tespera);//Viajando al supermercado Dia%.
 					comprarLeche(compa,tiempo);
 					messSNDT2.type = TYPE_2;
-					sleep(Tespera);
+					//Aviso que hay leche
 					msgsnd(queue,&messSNDT2,SIZE_MSG,0);
-					messSNDT3.type = TYPE_3;
-					for(int i=0;i<cantCompanieros;i++)//Desbloqueo los otros procesos que estaban esperando la leche.
-						msgsnd(queue,&messSNDT3,SIZE_MSG,0);
-									
+					sleep(Tespera);//Duerme la siesta despues del supermercado y la fafafa.
+																			
 				}
 				else{//Alguien fue a comprar
-					tiempoActual= time(NULL) -tiempo;
-					printf("%i : %i : No hay leche, pero otro fue a comprar.\n",tiempoActual, compa);
-					msgrcv(queue,&messRCV,SIZE_MSG,TYPE_3,0);
-					sleep(Tespera);
+					//tiempoActual= time(NULL) -tiempo;
+					//printf("%i : %i : No hay leche, pero otro fue a comprar.\n",tiempoActual, compa);
+					//sleep(5*Tespera);
 					
 				}
 				
@@ -139,12 +133,12 @@ int main(){
 		msgsnd(queueID,&messSND,SIZE_MSG,IPC_NOWAIT);
 	}
 	if(pid == 0){
-		companiero(tiempoInicial,aux+1,queueID,key,n);
+		companiero(tiempoInicial,aux+1,queueID,n);
 	}
 	else{
-			for (int i=0;i<n;i++)
+			for(int i=0;i<n;i++)
 				wait(NULL);
-		}
+	}
 	
 
 	
