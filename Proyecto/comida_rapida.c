5dd359610d626f0e3f0601713925003adb8d6050
Mati <matiaspippig@gmail.com>
2020-11-21 14:20:28 -0300

    Limpiado el codigo de comida_rapida.c y agregado un documento de txt con la documentacion del proyecto, pensado para reemplazarlo con un PDF cuando este terminada

--- Proyecto/comida_rapida.c
+++ Proyecto/comida_rapida.c
@@ -7,13 +7,6 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
-//variables globas para imitar los valores de los semaforos
-int empty = 0;
-int full = 0;
-int clean = 0;
-int dirty = 0;
-int asks = 0;
-int ans = 0;
 
 sem_t vacios; //lugares para platos
 sem_t ocupado; //lugares ocupados
@@ -26,13 +19,11 @@ void* cocinero(void* num_cocinero){
 	int numero_c = (int) num_cocinero;
 	while(1){
 		sem_wait(&vacios);
-		empty--;
 		//hace un plato
-		printf("Estoy cocinando. Cocinero:%i Lugares Vacios:%i\n",numero_c,empty);
+		printf("Estoy cocinando. Cocinero:%i \n",numero_c);
 		sleep(2);
-		printf("Plato listo para servir del Cocinero:%i Lugares Llenos:%i\n",numero_c,full);
+		printf("Plato listo para servir del Cocinero:%i \n",numero_c);
 		sem_post(&ocupado);
-		full++;
 	}
 	pthread_exit(NULL);
 	return NULL;
@@ -41,52 +32,39 @@ void* camarero(){
 	while(1){
 		sem_wait(&pedidos);
 		sem_wait(&ocupado);
-		asks--;
-		full--;
 		//recoge el plato
-		printf("Tome un plato de la cola  Pedidos Activos:%i  Lugares ocupados:%i y Vacios:%i \n",asks,full,empty);
+		printf("Tome un plato de la cola\n");
 		sem_post(&vacios);
-		empty++;
 		sleep(1);
-		printf("Cliente atendido Pedidos Atendidos:%i\n",ans);
+		printf("Cliente atendido\n");
 		sem_post(&atencion);
-		ans++;
 	}
 	pthread_exit(NULL);
 	return NULL;
 }
 void* cliente(void* num_cliente){
 	int numero_c = (int)num_cliente;
-	while(1){
-		printf("Esperando una mesa limpia.....Cliente:%i\n",numero_c);
-		sem_wait(&mesas_limpias);
-		clean--;
-		//elige la comida
-		sleep(1);
-		asks++;
-		sem_post(&pedidos);
-		//espera que lo atiendan
-		sem_wait(&atencion);
-		ans--;
-		//come
-		sleep(10);
-		//se va
-		printf("Adios Cliente:%i Mesas Limpias:%i Pedidos Activos:%i Pedidos Atendidos:%i Mesas Sucias:%i\n",numero_c,clean,asks,ans,dirty);
-		sem_post(&mesas_sucias);
-		dirty++;
-	}
+	printf("Esperando una mesa limpia.....Cliente:%i\n",numero_c);
+	sem_wait(&mesas_limpias);
+	//elige la comida
+	sleep(1);
+	sem_post(&pedidos);
+	//espera que lo atiendan
+	sem_wait(&atencion);
+	//come
+	//sleep(10);
+	//se va
+	printf("Adios Cliente:%i\n",numero_c);
+	sem_post(&mesas_sucias);
 	pthread_exit(NULL);
 	return NULL;
 }
 void* limpiador(){
 	while(1){
 		sem_wait(&mesas_sucias);
-		dirty--;
 		//limpia la mesa
 		sleep(2);
-		printf("Mesas limpias:%i Mesas sucias:%i\n",clean,dirty);
 		sem_post(&mesas_limpias);
-		clean++;
 	}
 	pthread_exit(NULL);
 	return NULL;
@@ -105,12 +83,6 @@ int main(){
 	sem_init(&pedidos,0,0);
 	sem_init(&atencion,0,0);
 	
-	empty = 10;
-	full=0;
-	clean=30;
-	dirty=0;
-	asks=0;
-	ans=0;
 	pthread_t hilos_clientes[clientes];
 	pthread_t hilos_limpiadores[limpiadores];
 	pthread_t hilos_cocineros[cocineros];
