85751ad37324a7f4db100a46e85f7b843616380a
Mati <matiaspippig@gmail.com>
2020-12-04 20:50:30 -0300

    Arreglado el programa de tareas_abc.c, agregue pipes a cambio de no recrear los hilos

--- Proyecto/src/tareas_abc.c
+++ Proyecto/src/tareas_abc.c
@@ -16,8 +16,11 @@ struct mensaje{
 }; typedef struct mensaje* msj_t;
 
 int pipe_a[2];
+int pipe_hilos_a[2];
 int pipe_b[2];
+int pipe_hilos_b[2];
 int pipe_c[2];
+int pipe_hilos_c[2];
 int pipe_final[2];
 
 msj_t crear_tarea_a(){
@@ -108,153 +111,150 @@ void coordinador_principal(){
 }
 
 void* tarea_a(void* args){
-	msj_t tarea = args;
-	enum tipos_tareas {Parcial=1, Total};
-	int tipo = tarea->tipo;
-	int color = tarea->informacion;
-	if(tipo == Parcial){
-		printf("Recibi una tarea Parcial para pintar de color: %s\n", colores[color]);
-		sleep(1*TIME_UNIT);
-	}
-	else{
-		if(tipo == Total){
-		printf("Recibi una tarea Total para pintar de color: %s\n", colores[color]);	
-		sleep(3*TIME_UNIT);
+	while(1){
+		msj_t tarea = malloc(sizeof(struct mensaje));
+		read(pipe_hilos_a[0],tarea,sizeof(struct mensaje));
+		enum tipos_tareas {Parcial=1, Total};
+		int tipo = tarea->tipo;
+		int color = tarea->informacion;
+		if(tipo == Parcial){
+			printf("Recibi una tarea Parcial para pintar de color: %s\n", colores[color]);
+			sleep(1*TIME_UNIT);
 		}
 		else{
-			printf("Recibi un tipo Invalido en A\n");
-			exit(2);
+			if(tipo == Total){
+			printf("Recibi una tarea Total para pintar de color: %s\n", colores[color]);	
+			sleep(3*TIME_UNIT);
+			}
+			else{
+				printf("Recibi un tipo Invalido en A\n");
+				exit(2);
+			}
 		}
+		int i = 0;
+		write(pipe_final[1],&i,sizeof(int));
 	}
-	void * ret=NULL;
-	free(args);
-	pthread_exit(ret);
 }
 void* tarea_b(void* args){
-	msj_t tarea = args;
-	enum tipos_tareas {Verificacion=1, Reparacion};
-	int tipo = tarea->tipo;
-	if(tipo == Verificacion){
-		printf("Recibi una tarea de Verificacion de Frenos\n");
-		sleep(1*TIME_UNIT);
-	}
-	else{
-		if(tipo == Reparacion){
-		printf("Recibi una tarea de Reparacion de Frenos\n");	
-		sleep(2*TIME_UNIT);
+	while(1){
+		msj_t tarea = malloc(sizeof(struct mensaje));
+		read(pipe_hilos_b[0],tarea,sizeof(struct mensaje));
+		enum tipos_tareas {Verificacion=1, Reparacion};
+		int tipo = tarea->tipo;
+		if(tipo == Verificacion){
+			printf("Recibi una tarea de Verificacion de Frenos\n");
+			sleep(1*TIME_UNIT);
 		}
 		else{
-			printf("Recibi un tipo Invalido en B\n");
-			exit(2);
+			if(tipo == Reparacion){
+			printf("Recibi una tarea de Reparacion de Frenos\n");	
+			sleep(2*TIME_UNIT);
+			}
+			else{
+				printf("Recibi un tipo Invalido en B\n");
+				exit(2);
+			}
 		}
+		int i = 1;
+		write(pipe_final[1],&i,sizeof(int));
 	}
-	void * ret=NULL;
-	free(args);
-	pthread_exit(ret);
 }
 void* tarea_c(void* args){
-	msj_t tarea = args;
-	enum tipos_tareas {Balanceo=1, Reparacion=2};
-	int tipo = tarea->tipo;
-	int ruedas = tarea->informacion;
-	if(tipo == Balanceo){
-		printf("Recibi una tarea de Balanceo y Rotacion de ruedas\n");
-		sleep(3*TIME_UNIT);
-	}
-	else{
-		if(tipo == Reparacion){
-		printf("Recibi una tarea para Reparar %i ruedas del vehiculo\n", ruedas);	
-		sleep(ruedas*TIME_UNIT);
+	while(1){
+		msj_t tarea = malloc(sizeof(struct mensaje));
+		read(pipe_hilos_c[0],tarea,sizeof(struct mensaje));	
+		enum tipos_tareas {Balanceo=1, Reparacion=2};
+		int tipo = tarea->tipo;
+		int ruedas = tarea->informacion;
+		if(tipo == Balanceo){
+			printf("Recibi una tarea de Balanceo y Rotacion de ruedas\n");
+			sleep(3*TIME_UNIT);
 		}
 		else{
-			printf("Recibi un tipo Invalido en C\n");
-			exit(2);
+			if(tipo == Reparacion){
+			printf("Recibi una tarea para Reparar %i ruedas del vehiculo\n", ruedas);	
+			sleep(ruedas*TIME_UNIT);
+			}
+			else{
+				printf("Recibi un tipo Invalido en C\n");
+				exit(2);
+			}
 		}
+		int i = 2;
+		write(pipe_final[1],&i,sizeof(int));
 	}
-	void * ret=NULL;
-	free(args);
-	pthread_exit(ret);
 }
 
 void coordinador_a(){
+	pthread_t *hilos_a;
+	hilos_a = malloc(sizeof(2 *sizeof(pthread_t)));
+	pthread_create(&hilos_a[0],NULL,tarea_a,NULL);
+	pthread_create(&hilos_a[1],NULL,tarea_a,NULL);
 	while(1){
 		int cant_tareas = 1;
 		int i = 0;
-		pthread_t *hilos_a;
 		while(i<cant_tareas){
 			msj_t tarea_corriente = malloc(sizeof(struct mensaje));
 			if(tarea_corriente == NULL)
 				exit(1);
 			read(pipe_a[0],tarea_corriente,sizeof(struct mensaje));
 			cant_tareas = tarea_corriente->cantidad;
-			hilos_a = malloc(sizeof(cant_tareas *sizeof(pthread_t)));
-			pthread_create(&hilos_a[i],NULL,tarea_a,tarea_corriente);
+			write(pipe_hilos_a[1],tarea_corriente,sizeof(struct mensaje));
 			i++;
 		}
-		printf("Se estan realizando %d tareas de tipo A\n",cant_tareas);
-		i=0;
-		while(i<cant_tareas){
-			pthread_join(hilos_a[i],NULL);
-			write(pipe_final[1],&i,sizeof(int));
-			i++;
-		}
-		
+		printf("Se estan realizando %d tareas de tipo A\n",cant_tareas);		
 	}
 }
 void coordinador_b(){
+	pthread_t *hilos_b;
+	hilos_b = malloc(sizeof(2 *sizeof(pthread_t)));
+	pthread_create(&hilos_b[0],NULL,tarea_b,NULL);
+	pthread_create(&hilos_b[1],NULL,tarea_b,NULL);
 	while(1){
 		int cant_tareas = 1;
 		int i = 0;
-		pthread_t *hilos_b;
 		while(i<cant_tareas){
 			msj_t tarea_corriente = malloc(sizeof(struct mensaje));
 			if(tarea_corriente == NULL)
 				exit(1);
 			read(pipe_b[0],tarea_corriente,sizeof(struct mensaje));
 			cant_tareas = tarea_corriente->cantidad;
-			hilos_b = malloc(sizeof(cant_tareas *sizeof(pthread_t)));
-			pthread_create(&hilos_b[i],NULL,tarea_b,tarea_corriente);
+			write(pipe_hilos_b[1],tarea_corriente,sizeof(struct mensaje));
 			i++;
 		}
 		printf("Se estan realizando %d tareas de tipo B\n",cant_tareas);		
 		i=0;
-		while(i<cant_tareas){
-			pthread_join(hilos_b[i],NULL);
-			write(pipe_final[1],&i,sizeof(int));
-			i++;
-		}
 	}
 }
 void coordinador_c(){
+	pthread_t *hilos_c;
+	hilos_c = malloc(sizeof(2 *sizeof(pthread_t)));
+	pthread_create(&hilos_c[0],NULL,tarea_c,NULL);
+	pthread_create(&hilos_c[1],NULL,tarea_c,NULL);
 	while(1){
 		int cant_tareas = 1;
 		int i = 0;
-		pthread_t *hilos_c;
 		while(i<cant_tareas){
 			msj_t tarea_corriente = malloc(sizeof(struct mensaje));
 			if(tarea_corriente == NULL)
 				exit(1);
 			read(pipe_c[0],tarea_corriente,sizeof(struct mensaje));
+			write(pipe_hilos_c[1],tarea_corriente,sizeof(struct mensaje));
 			cant_tareas = tarea_corriente->cantidad;
-			hilos_c = malloc(sizeof(cant_tareas*sizeof(pthread_t)));
-			pthread_create(&hilos_c[i],NULL,tarea_c,tarea_corriente);
 			i++;
 		}
 		printf("Se estan realizando %d tareas de tipo C\n",cant_tareas);
-		i=0;
-		while(i<cant_tareas){
-			pthread_join(hilos_c[i],NULL);
-			write(pipe_final[1],&i,sizeof(int));
-			i++;			
-		}
 	}
 }
 
 int main(){
 	srand(time(NULL));
 	pipe(pipe_a);
+	pipe(pipe_hilos_a);
 	pipe(pipe_b);
+	pipe(pipe_hilos_b);
 	pipe(pipe_c);
+	pipe(pipe_hilos_c);
 	pipe(pipe_final);
 	int pid_a = fork();
 	if(pid_a == 0){
